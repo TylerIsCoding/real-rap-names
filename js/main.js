24bfc7b3f9c3fa8e5956f52e3cd3fa9dfd2c5ad2
Tyler McKenna <tmckenna1988@gmail.com>
2023-03-03 16:14:14 -0500

    Linked JS folder and made output location

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 let button = document.querySelector('button');
+let output = document.querySelector('span');
+
+button.addEventListener('click', apiRequest);
+
+async function apiRequest() {
+  const rapperName = document.querySelector('input').value;
+  try {
+    const res = await fetch(
+      `https://lazy-pear-rooster-tux.cyclic.app/api/${rapperName}`
+    );
+    const data = await res.json();
+    console.log(data);
+    output.innerText = data.birthName;
+  } catch (error) {
+    console.error(error);
+  }
+}
