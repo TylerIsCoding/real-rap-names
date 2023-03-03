d12256854cd4eedbcbe63339834dbb3df6e98df8
Tyler McKenna <tmckenna1988@gmail.com>
2023-03-03 16:04:32 -0500

    Added process.env.PORT to the listen method

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get('/api/:rapperName', (request, response) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
